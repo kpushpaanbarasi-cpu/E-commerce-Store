8fbd40f2fb77dbd543e7ba4ec77fc6530b0be562
kpushpaanbarasi-cpu <kpushpaanbarasi@gmail.com>
2026-06-29 11:24:53 +0530

    import products.js into amazon.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,6 @@
 import{cart}from'../data/cart.js';
+import{products}from'../data/products.js';
+
 let productsHTML='';
 products.forEach((products)=>{
 productsHTML+=`<div class="product-container">
