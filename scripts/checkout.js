04e4374e1b9418d96f1cee8184ec7299e2e0760e
kpushpaanbarasi-cpu <kpushpaanbarasi@gmail.com>
2026-09-05 12:39:44 +0530

    practiced backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@ import { renderCheckout } from"./checkout/orderSummary.js";
 import {getTotal}from"./checkout/paymentSummary.js";
 import {saveUpdatestorage}from "./checkout/orderSummary.js";
 //import  '../data/cart-class.js';
+import '../data/backend.js';
 renderCheckout();
 getTotal();
 saveUpdatestorage();
