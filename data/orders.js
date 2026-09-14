420ce28545cbd107b7e4852ab0f3ca11fb08e0e8
kpushpaanbarasi-cpu <kpushpaanbarasi@gmail.com>
2026-09-14 21:22:49 +0530

    make the tracking page interactive

--- data/orders.js
+++ data/orders.js
@@ -1,9 +1,17 @@
 export const orders=localStorage.getItem('orders') ? JSON.parse(localStorage.getItem('orders')) : [];
+ export function save(){
+    localStorage.setItem('orders',JSON.stringify(orders));
+}
 export function addOrder(order){
     orders.unshift(order);
     save();
 }
-
- export function save(){
-    localStorage.setItem('orders',JSON.stringify(orders));
+export function getorderId(orderId){
+    let matchingOrder;
+    orders.forEach(order=>{
+    if(order.id===orderId){
+        matchingOrder=order;
+    }
+});
+    return matchingOrder;
 }
---
+++ scripts/checkout/tracking.js
@@ -0,0 +1,63 @@
+import{getorderId}from '../../data/orders.js';
+import { getproductId,loadProductsFetch } from '../../data/products.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import { orders } from '../../data/orders.js';
+async function loadPage(){
+  await loadProductsFetch();
+  const url= new URL(window.location.href);
+  const orderId=url.searchParams.get('orderId');
+  const productId=url.searchParams.get('productId');
+  const order=getorderId(orderId);
+  const product=getproductId(productId);
+    let productDetails;
+  order.products.forEach((details) => {
+    if (details.productId === product.id) {
+      productDetails = details;
+    }
+  });
+  const today= dayjs();
+  const orderTime=dayjs(order.orderTime);
+  const deliveryTime=dayjs(productDetails.estimatedDeliveryTime);
+  const percentProgress=((today.diff(orderTime))/(deliveryTime.diff(orderTime)))*100;
+  const trackingHTML=`
+   <div class="order-tracking">
+        <a class="back-to-orders-link link-primary" href="orders.html">
+          View all orders
+        </a>
+
+        <div class="delivery-date">
+          Arriving on ${
+        dayjs(productDetails.estimatedDeliveryTime).format('dddd, MMMM D')
+      }
+        </div>
+
+        <div class="product-info">
+    ${product.name}
+        </div>
+
+        <div class="product-info">
+          Quantity: ${productDetails.quantity}
+        </div>
+
+        <img class="product-image" src="${product.image}">
+
+        <div class="progress-labels-container">
+          <div class="progress-label ${percentProgress<30?'current-status':''}">
+            Preparing
+          </div>
+          <div class="progress-label ${percentProgress>=30&&percentProgress<100?'current-status':''}">
+            Shipped
+          </div>
+          <div class="progress-label ${percentProgress>=100?'current-status':''}">
+            Delivered
+          </div>
+        </div>
+
+        <div class="progress-bar-container">
+          <div class="progress-bar" style="width: ${percentProgress}%;"></div>
+        </div>`;
+
+  document.querySelector('.js-order-tracking').innerHTML = trackingHTML;
+}
+loadPage();
+
